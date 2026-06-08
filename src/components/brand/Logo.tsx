7244f0be5646e3c134c9278743e95f957e6b1fd5
Andrea Diaz <thouthamaashrithsai@gmail.com>
2026-06-08 13:23:47 -0400

    Update GEDI logo

--- src/components/brand/Logo.tsx
+++ src/components/brand/Logo.tsx
@@ -9,9 +9,9 @@ export default function Logo({ compact = false, className = '' }: LogoProps) {
   return (
     <Link to="/" className={`inline-flex min-w-0 items-center ${className}`} aria-label="GEDI home">
       <img
-        src="/gedi_logo/gedi-logo-transparent.png"
+        src="/gedi_logo/gedi-logo-purple.svg"
         alt="GEDI Global Early Detection Initiative"
-        className={compact ? 'h-10 max-w-[190px] object-contain sm:h-11 sm:max-w-[230px] md:h-14 md:max-w-none' : 'h-14 max-w-[230px] object-contain sm:h-16 md:h-20 md:max-w-none'}
+        className={compact ? 'h-10 max-w-[190px] object-contain sm:h-11 sm:max-w-[230px] md:h-14 md:max-w-[260px]' : 'h-14 max-w-[240px] object-contain sm:h-16 sm:max-w-[280px] md:h-20 md:max-w-[345px]'}
       />
     </Link>
   );
